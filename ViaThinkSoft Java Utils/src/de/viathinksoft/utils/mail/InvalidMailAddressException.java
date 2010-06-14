975d7732b9ec31b33921dc3beab1508aaaa41bc4
Daniel Marschall <info@daniel-marschall.de>
2010-06-14 13:05:17 +0000

    InvalidMailAddressException wird nicht mehr verwendet
    git-svn-id: https://svn.viathinksoft.com/svn/javautils@10 80292b2e-1f3e-42c1-a96e-e7bd188648c5

--- ViaThinkSoft Java Utils/src/de/viathinksoft/utils/mail/InvalidMailAddressException.java
+++
@@ -1,8 +0,0 @@
-package de.viathinksoft.utils.mail;
-
-@Deprecated
-public class InvalidMailAddressException extends Exception {
-
-	private static final long serialVersionUID = -3748914913077717465L;
-
-}
