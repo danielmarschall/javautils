e2f7f955638f1d0f73491171bc470aff93149ff5
Daniel Marschall <info@daniel-marschall.de>
2010-06-14 09:48:33 +0000

    EMailPreprocessor.java added
    git-svn-id: https://svn.viathinksoft.com/svn/javautils@8 80292b2e-1f3e-42c1-a96e-e7bd188648c5

---
+++ ViaThinkSoft Java Utils/src/de/viathinksoft/utils/mail/EMailPreprocessor.java
@@ -0,0 +1,24 @@
+package de.viathinksoft.utils.mail;
+
+/**
+ * This class contains a function which "preproceses an email address. Following
+ * steps will be performed: 1. The email address will be trimmed (in case of
+ * user inputs) 2. E-Mail-Addresses with internationalized domain names will be
+ * converted into ASCII compatible punycode (the local part will be left as it
+ * is!)
+ * 
+ * @author Daniel Marschall
+ * 
+ */
+public class EMailPreprocessor {
+	
+	public static String preprocess(String eMailAddress) throws InvalidMailAddressException {
+		EMailAddress email = new EMailAddress(eMailAddress);
+		
+		return email.getMailAddressPunycodedDomain();
+	}
+	
+	private EMailPreprocessor() {
+	}
+
+}
