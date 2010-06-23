ea59e559ee5552dee8a8d5a61d143bd7b02e2fdc
Daniel Marschall <info@daniel-marschall.de>
2010-06-23 10:28:30 +0000

    Mailing Tests nun mit Mock Objects
    git-svn-id: https://svn.viathinksoft.com/svn/javautils@21 80292b2e-1f3e-42c1-a96e-e7bd188648c5

--- ViaThinkSoft Java Utils/test/eMailTests/TestConfiguration.java
+++ ViaThinkSoft Java Utils/test/eMailTests/TestConfiguration.java
@@ -4,32 +4,14 @@ package eMailTests;
 // Ginge auch als Singleton
 
 public final class TestConfiguration {
+	
+	// Gott habe Mitleid mit dem Eigentümer dieser E-Mail-Adresse...
+	// Eine Wegwerfadresse für manuelle Tests kann auf
+	// www.10minutemail.com erstellt werden.
+	private static final String SPAMMING_MAIL_ADDRESS = "a1175972@bofthew.com";
+	
 	public static String getSpamAddress() {
-		// Gott habe Mitleid mit dem Eigentümer dieser E-Mail-Adresse...
-		return "b973768@owlpic.com";
-
-		// Eine Wegwerfadresse für manuelle Tests kann auf
-		// www.10minutemail.com erstellt werden.
-	}
-	
-	public static String getSmtpHost() {
-		return "";
-	}
-	
-	public static String getSmtpUsername() {
-		return "";
-	}
-	
-	public static String getSmtpPassword() {
-		return "";
-	}
-	
-	public static int getSmtpPort() {
-		return 25;
-	}
-	
-	public static String getMailFrom() {
-		return getSpamAddress();
+		return SPAMMING_MAIL_ADDRESS;
 	}
 	
 	private TestConfiguration() {
